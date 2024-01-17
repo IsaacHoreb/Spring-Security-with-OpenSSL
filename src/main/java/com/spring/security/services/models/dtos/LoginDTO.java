00a2db98a7845e4b8d5e2279f84bbcd1f0695b42
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2024-01-17 14:46:53 -0600

    Agregados para los DTO

--- src/main/java/com/spring/security/services/models/dtos/LoginDTO.java
+++ src/main/java/com/spring/security/services/models/dtos/LoginDTO.java
@@ -1,4 +1,15 @@
 package com.spring.security.services.models.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginDTO {
+
+    private String email;
+    private String password;
+
 }
--- src/main/java/com/spring/security/services/models/dtos/ResponseDTO.java
+++ src/main/java/com/spring/security/services/models/dtos/ResponseDTO.java
@@ -1,5 +1,15 @@
 package com.spring.security.services.models.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ResponseDTO {
-    
+
+    private int numOfErrors;
+    private String message;
+
 }
