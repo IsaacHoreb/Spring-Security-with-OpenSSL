67879a9cc1461d09a9a7241c3e5dd071681916c3
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2024-01-18 14:17:57 -0600

    Terminacion del controllador

--- src/main/java/com/spring/security/controllers/UserControllers.java
+++ src/main/java/com/spring/security/controllers/UserControllers.java
@@ -1,4 +1,27 @@
 package com.spring.security.controllers;
 
+import com.spring.security.persistence.entity.UserEntity;
+import com.spring.security.services.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/user")
 public class UserControllers {
+
+    @Autowired
+    IUserService userService;
+
+    @GetMapping("/find-all")
+    private ResponseEntity<List<UserEntity>> getAllUsers() {
+        return new ResponseEntity<>(userService.findAllUsers(), HttpStatus.OK);
+    }
+
+
 }
