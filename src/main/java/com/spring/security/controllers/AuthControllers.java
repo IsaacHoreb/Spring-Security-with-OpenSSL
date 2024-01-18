a49a53190e91f1fd6f677e1c5985b166140c07eb
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2024-01-18 13:01:23 -0600

    Correccion de nombre

---
+++ src/main/java/com/spring/security/controllers/AuthControllers.java
@@ -0,0 +1,13 @@
+package com.spring.security.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/auth")
+public class AuthControllers {
+
+    
+
+
+}
