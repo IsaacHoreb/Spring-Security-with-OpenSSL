64174975adc8a67c07ed38b3c58cc8f0cf1ad802
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2024-01-18 09:51:33 -0600

    Agregado para el Jwt en el filtro de autorizacion

---
+++ src/main/java/com/spring/security/security/JWTAuthorizationFilter.java
@@ -0,0 +1,60 @@
+package com.spring.security.security;
+
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.spring.security.services.IJWTUtilityService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.text.ParseException;
+import java.util.Collections;
+
+//DESPUES DE TERMINAR NOS VAMOS A SECURITYCONFIG
+public class JWTAuthorizationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    IJWTUtilityService jwtUtilityService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String header = request.getHeader("Authorization"); //Obtengo al pedir
+
+        //Condiccion no cumple
+        if (header == null || header.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = header.substring(7); //Quita los primero 7
+
+        //
+        try {
+
+            JWTClaimsSet claims = jwtUtilityService.parseJWT(token);
+
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(
+                            claims.getSubject(),
+                            null,
+                            Collections.emptyList());
+
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException | ParseException | JOSEException e) {
+            throw new RuntimeException(e);
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
+}
